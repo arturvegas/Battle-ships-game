9058c146cee65d45fa509e874f78700e95d17dfb
Artur Vegas <artur.arti2008@gmail.com>
2024-03-13 17:53:39 +0100

    Dodaj pliki projektów.

---
+++ Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battleshipsgame
+{
+    internal class Menu
+    {
+        public void ShowMenu()
+        {
+            Console.WriteLine("============");
+            Console.WriteLine("   STATKI   ");
+            Console.WriteLine("============");
+        }
+
+        public void NextPlayer()
+        {
+            Console.WriteLine("Kliknij losowy przycisk jeżeli gracz się zmienił");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
